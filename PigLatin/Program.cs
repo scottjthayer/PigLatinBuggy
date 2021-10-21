72860b7b57e095545764c800ae820bdf45bcb735
scott <scottjthayer@gmail.com>
2021-10-21 08:58:47 -0400

    this was annoying. but okay here is my commit

--- PigLatin/Program.cs
+++ PigLatin/Program.cs
@@ -7,83 +7,87 @@ namespace PigLatin
     {
         static void Main(string[] args)
         {
-            string userInput = GetInput("Please input a word or sentence to translate to pig Latin");
-
-            string translation = ToPigLatin(userInput);
-            Console.WriteLine(translation);
-        }
-
-        public static string GetInput(string prompt)
-        {
-            Console.WriteLine(prompt);
+            Translate translate = new Translate();
+            Console.WriteLine("Please input a word or sentence to translate to pig Latin");
             string input = Console.ReadLine().ToLower().Trim();
-            return input;
+
+            if (translate.CheckForSpecialCharacters(input) == true)
+            {
+                Console.WriteLine(input);
+            }
+            else if (translate.CheckForSpecialCharacters(input) == false)
+            {
+                string translation = translate.ToPigLatin(input);
+                Console.WriteLine(translation);
+            }
         }
 
-        public static bool IsVowel(char c)
-        {
-            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
-            
-            return c.ToString() == vowels.ToString();
-        }
 
-        public static string ToPigLatin(string word)
-        {
-            char[] specialChars = { '@', '.', '-', '$', '^', '&' };
-            word = word.ToLower();
-            foreach(char c in specialChars)
-            {
-                foreach(char w in word)
-                {
-                    if (w == c)
-                    {
-                        Console.WriteLine("That word has special characters, we will return it as is");
-                        return word;
-                    }
-                }
-                
-            }
+        //public static bool IsVowel(char c)
+        //{
+        //    char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
 
-            bool noVowels = true;
-            foreach(char letter in word)
-            {
-                if (IsVowel(letter))
-                {
-                    noVowels = false;
-                }
-            }
+        //    return c.ToString() == vowels.ToString();
+        //}
 
-            if (noVowels)
-            {
-                return word; 
-            }
+        //public static string ToPigLatin(string word)
+        //{
+        //    char[] specialChars = { '@', '.', '-', '$', '^', '&' };
+        //    word = word.ToLower();
+        //    foreach(char c in specialChars)
+        //    {
+        //        foreach(char w in word)
+        //        {
+        //            if (w == c)
+        //            {
+        //                Console.WriteLine("That word has special characters, we will return it as is");
+        //                return word;
+        //            }
+        //        }
 
-            char firstLetter = word[0];
-            string output = "placeholder";
-            if (IsVowel(firstLetter) == true)
-            {
-                output = word + "ay";
-            }
-            else
-            {
-                int vowelIndex = -1;
-                //Handle going through all the consonants
-                for (int i = 0; i <= word.Length; i++)
-                {
-                    if (IsVowel(word[i]) == true)
-                    {
-                        vowelIndex = i;
-                        break;
-                    }
-                }
+        //    }
 
-                string sub = word.Substring(vowelIndex + 1);
-                string postFix = word.Substring(0, vowelIndex -1);
+        //    bool noVowels = true;
+        //    foreach(char letter in word)
+        //    {
+        //        if (IsVowel(letter))
+        //        {
+        //            noVowels = false;
+        //        }
+        //    }
 
-                output = sub + postFix + "ay";
-            }
+        //    if (noVowels)
+        //    {
+        //        return word; 
+        //    }
 
-            return output;
-        }
+        //    char firstLetter = word[0];
+        //    string output = "placeholder";
+        //    if (IsVowel(firstLetter) == true)
+        //    {
+        //        output = word + "ay";
+        //    }
+        //    else
+        //    {
+        //        int vowelIndex = -1;
+        //        //Handle going through all the consonants
+        //        for (int i = 0; i <= word.Length; i++)
+        //        {
+        //            if (IsVowel(word[i]) == true)
+        //            {
+        //                vowelIndex = i;
+        //                break;
+        //            }
+        //        }
+
+        //        string sub = word.Substring(vowelIndex + 1);
+        //        string postFix = word.Substring(0, vowelIndex -1);
+
+        //        output = sub + postFix + "ay";
+        //    }
+
+        //    return output;
+
+
     }
 }
---
+++ PigLatin/Translate.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PigLatin
+{
+    class Translate
+    {
+        public static bool IsVowel(char c)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            foreach (char ch in vowels)
+            {
+                if (ch == c)
+                { return true; }
+            }
+            return false;
+        }
+        public bool CheckForSpecialCharacters(string word)
+        {
+            char[] specialChars = { '@', '.', '-', '$', '^', '&' };
+            foreach (char c in specialChars)
+            {
+                foreach (char w in word)
+                {
+                    if (w == c)
+                    {
+                        Console.WriteLine("That word has special characters, we will return it as is");
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public string ToPigLatin(string word)
+        {
+            //I don't like this.
+
+
+            //this is strange depending on the understanding of pig latin. Some sites state that you still ad -ay if no vowels are found. But to each their own.              
+            char firstLetter = word[0];
+            bool vowelFirst = IsVowel(firstLetter);
+            if (vowelFirst == true)
+            {
+                return word + "way";
+            }
+            else if (vowelFirst == false)
+            {
+                return TranslateConsonantWord(word);
+            }
+            else
+            {
+                foreach (char letter in word)
+                {
+                    bool noVowels = IsVowel(letter);
+                    if (noVowels != true)
+                    {
+
+                        break;
+                    }
+
+                }
+                return word;
+            }
+        }
+        public string TranslateConsonantWord(string word)
+        {
+            int vowelIndex = 0;
+            //Handle going through all the consonants
+            //IS IT THOUGH>??
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (IsVowel(word[i]) == true)
+                {
+                    vowelIndex = i - 1;
+                    break;
+                }
+            }
+
+            string sub = word.Substring(vowelIndex + 1);
+            string postFix = word.Substring(0, vowelIndex + 1);
+
+            return sub + postFix + "ay";
+        }
+    }
+}
